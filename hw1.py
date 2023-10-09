cae4a35ef510451875c56b37c928195fe9d48e83
henriquedaponte <henriquedaponte@gmail.com>
2023-10-09 16:32:48 -0700

    created helper functions to simplify problem solution

--- hw1.py
+++ hw1.py
@@ -3,15 +3,7 @@ import numpy as np
 import time
 import random
 
-# ==================== Problem 1.a ====================
-
-# Defining the graph (used the same graph outlined in the homework)
-h = [1, 1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 7, 8, 9] # head nodes
-t = [2, 4, 8, 3, 7, 4, 6, 5, 6, 8, 7, 8, 9, 3] # tail nodes
-c = [10, 10, 1, 10, 1, 10, 1, 1, 12, 12, 12, 12, 6, 4] # column vector of edge capacities.
-s = np.array([1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]) # column vector with a 1 for every edge leaving the source and a 0 for every other edge
-names = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'] # names of the nodes
-
+# ==================== Helper Functions ====================
 
 # Function to compute the node-edge incidence matrix for a directed graph
 def incidence_matrix(h, t, nodes):
@@ -29,6 +21,66 @@ def incidence_matrix(h, t, nodes):
         
     return A
 
+def generate_all_connected_graph(n_nodes):
+    """Generate an all-connected graph with random capacities."""
+    h, t, weights = [], [], []
+    for i in range(1, n_nodes + 1):
+        for j in range(i + 1, n_nodes + 1):
+            h.append(i)
+            t.append(j)
+            weights.append(random.randint(1, 10))
+    return h, t, weights
+
+def computeS(h, c):
+    '''
+    Function to compute the column vector s 
+    with a 1 for every edge leaving the source 
+    and a 0 for every other edge
+    '''
+    s = np.zeros(len(c))
+
+    for i in range(len(h)):
+        if h[i] == 1:
+            s[i] = 1
+
+    return s
+
+def computeL(A, source, target):
+    '''
+    Function to compute the matrix L 
+    by removing rows corresponding to source and destination
+    '''
+    L = np.delete(A, [source-1, target-1], 0)
+
+    return L
+
+def solve_max_flow(s, L, c):
+    """Solve the max flow problem for the given graph."""
+    f = cp.Variable(len(c)) # Defining decision variable 
+
+    objective = cp.Maximize(s.T @ f) # Defining the objective function
+
+    constraints = [L @ f == 0, 0 <= f, f <= c] # Defining the constraints
+
+    problem = cp.Problem(objective, constraints) # Defining the problem
+
+    problem.solve() # Solving the problem
+
+    maxFlow = round(problem.value) # Optimal value (max flow)
+    maxFlowEdges = np.around(f.value) # Optimal flow on each edge
+
+
+# ==================== Problem 1.a ====================
+
+# Defining the graph (used the same graph outlined in the homework)
+h = [1, 1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 7, 8, 9] # head nodes
+t = [2, 4, 8, 3, 7, 4, 6, 5, 6, 8, 7, 8, 9, 3] # tail nodes
+c = [10, 10, 1, 10, 1, 10, 1, 1, 12, 12, 12, 12, 6, 4] # column vector of edge capacities.
+s = computeS(h, c) # column vector with a 1 for every edge leaving the source and a 0 for every other edge
+names = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'] # names of the nodes
+
+print(s)
+
 # Computing the node-edge incidence matrix for the graph
 nodes = list(range(1, len(names) + 1))
 A = incidence_matrix(h, t, nodes)
@@ -39,57 +91,25 @@ source = 1  # 'A'
 target = 9  # 'I'
 
 # Computing L by removing rows corresponding to source and destination
-L = np.delete(A, [source-1, target-1], 0)
+L = computeL(A, source, target)
 
 # CVXPY Max Flow Problem Formulation as linear program
 
-# ASK IN OH !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-f = cp.Variable(len(c)) # Defining decision variable 
-
-objective = cp.Maximize(s.T @ f) # Defining the objective function
-
-constraints = [L @ f == 0, 0 <= f, f <= c] # Defining the constraints
-
-problem = cp.Problem(objective, constraints) # Defining the problem
-
-problem.solve() # Solving the problem
+maxFlow, maxFlowEdges = solve_max_flow(s, L, c)
 
 # Printing the results
-print("Optimal value (max flow):", round(problem.value))
-print("Optimal flow on each edge:", np.around(f.value))
+# print("Optimal value (max flow):", maxFlow)
+# print("Optimal flow on each edge:", maxFlowEdges)
 
 
-'''
 
 # ==================== Problem 1.b ====================
 
 
-# Function to compute the node-edge incidence matrix for a directed graph
-def incidence_matrix(s, t, nodes):
-    m = len(nodes)
-    n = len(s)
-    A = np.zeros((m, n))
-    
-    for j in range(n):
-        # Find index of the start node (tail) of edge j
-        tail = nodes.index(s[j])
-        # Find index of the end node (head) of edge j
-        head = nodes.index(t[j])
-        A[tail, j] = 1
-        A[head, j] = -1
-        
-    return A
 
-def generate_all_connected_graph(n_nodes):
-    """Generate an all-connected graph with random capacities."""
-    s, t, weights = [], [], []
-    for i in range(1, n_nodes + 1):
-        for j in range(i + 1, n_nodes + 1):
-            s.append(i)
-            t.append(j)
-            weights.append(random.randint(1, 10))
-    return s, t, weights
 
+
+'''
 def solve_max_flow(s, t, weights, source=1, target=None):
     """Solve the max flow problem for the given graph."""
     if not target:
